ada69966fdd04525822c3f8d015985b81b363ccd
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-29 14:01:27 +0200

    exercice 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let lastName;
 let firstName;
 let city;
-alert ('Nom:'+ lastName 'Prénom:'+firstName 'Ville:'+city);
 lastName = 'Doe';
-firstName ='John';
-city ='New-York';
+firstName = 'John';
+city = 'New-York';
+alert('Nom : ' + lastName + '\nPrénom : ' + firstName + '\nVille : ' +  city);
