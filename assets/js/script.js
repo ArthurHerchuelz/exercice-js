950b894a44b87c706b38e6687c17a3a0e7060435
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-29 17:31:41 +0200

    exercice 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-var firstnameElt = document.getElementById("firstname");
-firstnameElt.value = "MonPseudo";
 
 document.getElementById('submit').onclick = function resumeForm() {
-  alert('bonjour !');
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  let city = document.getElementById("city").value;
+  alert('Nom : ' + lastname + '\nPrénom : ' + firstname + '\nVille : ' +  city);
 };
