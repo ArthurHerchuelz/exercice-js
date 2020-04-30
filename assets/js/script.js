d1d6a0a346cdeaadadd989ddb9da426273fae8bd
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-30 14:27:22 +0200

    exercice 1partie2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-function calcul() {
-var y = document.getElementById('number1').value;
-var x = document.getElementById('number2').value;
-var quotient = Math.floor(y/x);
-var remainder = y % x;
-alert(remainder);
-}
+
