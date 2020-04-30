c89f54946d516cbac820458a00e756156c8e5599
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-30 15:19:38 +0200

    1er test

---
+++ exercice4/voyage&co/assets/js/script.js
@@ -0,0 +1,15 @@
+("#block1").hide();
+("#block2").hide();
+
+// $(function () {
+//         $(window).scroll(function () {
+//              if ($(this).scrollTop() > 400 ) {
+//                 $('#block1').fadeIn(500);
+//             } else {
+//                 $('#block1').fadeOut(500);
+//             }
+//
+//             if ($(this).scrollTop() > 650 ) {
+//                 $('#block2').fadeIn(300);
+//             }
+//     });
