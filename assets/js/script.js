1d210753348e3031fba514a228ab689c2c55256f
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-30 10:45:26 +0200

    exercice6 calcul

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+// Création de la fonction de calcul
+function calculation() {
+// Récupération des valeurs
+var shoeSize = document.getElementById('shoesize').value;
+var yearBirth = document.getElementById('yearBirth').value;
+alert((shoeSize * 2 + 5) * 50 - yearBirth + 1769)
 
+}
