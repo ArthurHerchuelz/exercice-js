324506562b33bfd31e7f3a7a08c31f10f7c0dd53
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-04-30 11:27:50 +0200

    fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-// Création de la fonction de calcul et action onclick
-document.getElementById('submit').onclick = function calculation() {
-// Récupération des valeurs
-let shoeSize = document.getElementById('shoeSize').value;
-let yearBirth = document.getElementById('yearBirth').value;
-// création de l'alerte
-alert((shoeSize * 2 + 5) * 50 - yearBirth + 1769);
+document.getElementById('submit').onclick = function autorisation() {
+  let age = document.getElementById('age').value;
+   if (age < 18) {
+alert('Vous êtes mineur!');
+}
+  else {
+alert('Vous êtes majeur!')
+}
 };
