dd7edf11c4d802fcaa244a6142d33b8a0dd61c3d
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-04 10:57:43 +0200

    base

---
+++ exercice4/voyage&co/assets0/js/script.js
@@ -0,0 +1,9 @@
+let lastName;
+let firstName;
+let city;
+lastName = 'Doe';
+firstName = 'John';
+city = 'New-York';
+alert('Nom : ' + lastName + '\nPrénom : ' + firstName + '\nVille : ' +  city);
+
+document.getElementById(#Block1).style.display = none;
